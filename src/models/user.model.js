3fd576fcb989cb3a495198227939535c1d49d066
Aadityaa45 <aaditya.joshi@avantika.edu.in>
2025-04-20 00:25:57 +0530

    jwt bcrypt learning

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
-import mongoose,{Schema} from "mongoose"
+import mongoose,{Schema} from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const UserSchema = new Schema({
     username:{
@@ -48,6 +50,74 @@ const UserSchema = new Schema({
 }
 )
 
+UserSchema.pre("save",async function (next){  //here pre means just before saving the information in databse activate this hook
+    if(this.isModified("password")){
+        this.password = bcrypt.hash(this.password,10)  //yeh jo hash function h yeh do chije leta h ek to kya karna h or kitne round of algorithm lagane h 
+        next()
+    }
+    else{
+        return next();
+    }
+    
+})
+//here we dont use arrow function because in arrow function we doesnt have the access of the "this" keyword and here we use it thats why we are going with a simple function here by passing the next flag
+//here pre is a middelware hook
 
+
+UserSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password)   //this compare method always return true or false 
+}
+
+
+
+UserSchema.methods.generateRefreshToken = async function(){
+//     🔏 jwt.sign() = turns user info into a secure token you can send to clients.
+// Only your backend knows how to make or verify it using your secret
+   return jwt.sign({    //Basically, it takes your data (called "payload") and signs it with a secret key, so no one can tamper with it.
+        _id : this._id,
+
+    }),
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+}
+UserSchema.methods.generateAccessToken = async function(){
+    return jwt.sign({    //Basically, it takes your data (called "payload") and signs it with a secret key, so no one can tamper with it.
+        _id : this._id,
+        email:this.email,
+        username:this.username,
+        fullname : this.Fullname
+    }),
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+}
 export const User = mongoose.model("User",UserSchema);
+
+
+
+//jwt token is a bearer token that measn agar yeh token kisi ke pass bhi hoga use me data de dunga 
+
+//lets explore about refresh token and access tokens
+
+// How they work together (Real-world flow):
+// ✅ You login
+
+// 🔄 Server sends:
+
+// Access Token (short life)
+
+// Refresh Token (long life)
+
+// ✅ You use the Access Token for API requests
+
+// ⌛ Access token expires
+
+// 🔁 Client sends the Refresh Token to the server
+
+// 🔄 Server verifies and gives you a new Access Token
+
+
 
